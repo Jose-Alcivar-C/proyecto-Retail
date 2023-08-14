37e9c36275f997a861be602cf3c510c4a4be17b6
José Alcivar C <josealci@espol.edu.ec>
2023-08-14 14:13:11 -0500

    version011

--- deteccion.py
+++ deteccion.py
@@ -1,10 +1,11 @@
 import gradio as gr
 import cv2
+from recursos import textoSalida, colorCaja
 
 from ultralytics import YOLO
 
 #Cargar modelo entrenado
-modelo_entrenado = "./runs/yolov8m_test_01/weights/best.pt"
+modelo_entrenado = "./runs/yolov8m_test_08/weights/best.pt"
 
 model = YOLO(modelo_entrenado)
 
@@ -18,26 +19,38 @@ def show_results(loaded_image):
     #Se carga la imagen usando openCV para poder editarla
     image = cv2.imread(loaded_image)
     #Se recorre cada boundingBox detectado y para cada uno se pinta un rectangulo y se escribe un id.
+    
+    salida = dict()
+
     for i, det in enumerate(results.boxes.xyxy):
+        
+        id_detectado = results.names.get(results.boxes.cls[i])
+
         cv2.rectangle(image,
                       (int(det[0]), int(det[1])),
                       (int(det[2]), int(det[3])),
-                      color=(0, 0, 255),
-                      thickness=2,
+                      color=colorCaja(id_detectado),
+                      thickness=4,
                       lineType=cv2.LINE_AA
                      )
-        id_detectado = results.names.get(results.boxes.cls[i])
+
+        if(str(id_detectado)) in salida:
+            salida[str(id_detectado)] = salida[str(id_detectado)]+1
+        else:
+            salida[str(id_detectado)] = 1
+
         cv2.putText(image,
                     text =f"id:{str(id_detectado)}",
                     org=(int(det[0]), int(det[1])),
                     fontFace =cv2.FONT_HERSHEY_SIMPLEX,
                     fontScale=2,
-                    color=(0,0,255),
-                    thickness=1,
+                    color=colorCaja(id_detectado),
+                    thickness=4,
                     lineType=cv2.LINE_AA
                    )
     #Se retornan las 2 salidas definidas(imagen y texto): la imagen resultante (image) y un texto indicando cuantos boundingBox se encontraron
-    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB), len(results.boxes)
+    #return cv2.cvtColor(image, cv2.COLOR_BGR2RGB), len(results.boxes)
+    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB), textoSalida(salida)
 
 
 inputs = [gr.components.Image(type="filepath", label="Input Image"),
--- recursos.py
+++ recursos.py
@@ -44,5 +44,27 @@ def plot_results(file='path/to/results.csv', dir=''):
     plt.close()
 
 
+def textoSalida(diccionario):
+    
+    impresion = ""
 
-plot_results("./runs/yolov8m_test_01/results.csv")
+    for dato in diccionario:
+        
+        impresion = impresion + f"Productos con id {dato}, hay {str(diccionario[dato])}\n"
+
+    return impresion
+
+
+def colorCaja(idDetectado):
+
+    colores = {
+        "75":(0, 0, 255), 
+        "78":(0, 189, 255), 
+        "79":(0, 255, 158), 
+        "125":(135, 255, 0), 
+        "127":(255, 162, 0), 
+        "128":(255, 0, 135), 
+        "140":(151, 0, 255)
+        }
+
+    return colores[str(idDetectado)]
