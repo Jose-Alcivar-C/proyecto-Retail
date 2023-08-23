59e0bf8f6c3aab400bd2b1a9e8383a813367d2b2
José Alcivar C <josealci@espol.edu.ec>
2023-08-23 03:52:45 -0500

    version014

--- deteccion.py
+++ deteccion.py
@@ -5,7 +5,7 @@ from recursos import textoSalida, colorCaja
 from ultralytics import YOLO
 
 #Cargar modelo entrenado
-modelo_entrenado = "./runs/yolov8m_test_08/weights/best.pt"
+modelo_entrenado = "./modelos_finales/yolov8x_retail/weights/last.pt"
 
 model = YOLO(modelo_entrenado)
 
@@ -13,44 +13,30 @@ model = YOLO(modelo_entrenado)
 #La interfaz solo recibe una entrada (La imagen ingresada en el cargador de path de imagenes), por lo
 # q ue solo se define un parametro de entrada en la funcion.
 def show_results(loaded_image):
+    
     #Se generan las salidas (detecciones) pidiendo al modelo que prediga a partir de la imagen de entrada
-    outputs = model.predict(source=loaded_image)
-    results = outputs[0].cpu().numpy()
-    #Se carga la imagen usando openCV para poder editarla
-    image = cv2.imread(loaded_image)
-    #Se recorre cada boundingBox detectado y para cada uno se pinta un rectangulo y se escribe un id.
+    outputs = model.predict(source=loaded_image)[0]
+    
+    ploteo = outputs.plot()
+    
+    results = outputs.cpu().numpy()
     
     salida = dict()
+
+    #Se recorre cada boundingBox detectado y para cada uno se pinta un rectangulo y se escribe un id.
 
     for i, det in enumerate(results.boxes.xyxy):
         
         id_detectado = results.names.get(results.boxes.cls[i])
-
-        cv2.rectangle(image,
-                      (int(det[0]), int(det[1])),
-                      (int(det[2]), int(det[3])),
-                      color=colorCaja(id_detectado),
-                      thickness=4,
-                      lineType=cv2.LINE_AA
-                     )
 
         if(str(id_detectado)) in salida:
             salida[str(id_detectado)] = salida[str(id_detectado)]+1
         else:
             salida[str(id_detectado)] = 1
 
-        cv2.putText(image,
-                    text =f"id:{str(id_detectado)}",
-                    org=(int(det[0]), int(det[1])),
-                    fontFace =cv2.FONT_HERSHEY_SIMPLEX,
-                    fontScale=2,
-                    color=colorCaja(id_detectado),
-                    thickness=4,
-                    lineType=cv2.LINE_AA
-                   )
     #Se retornan las 2 salidas definidas(imagen y texto): la imagen resultante (image) y un texto indicando cuantos boundingBox se encontraron
     #return cv2.cvtColor(image, cv2.COLOR_BGR2RGB), len(results.boxes)
-    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB), textoSalida(salida)
+    return cv2.cvtColor(ploteo, cv2.COLOR_BGR2RGB), textoSalida(salida)
 
 
 inputs = [gr.components.Image(type="filepath", label="Input Image"),
@@ -58,15 +44,13 @@ inputs = [gr.components.Image(type="filepath", label="Input Image"),
 outputs= [gr.components.Image(type="numpy", label="Output Image"),
           gr.Textbox(label="Total:")
          ]
-examples  = "./output/test/images/"
+#examples  = ""
 
 interface = gr.Interface(fn=show_results, 
                          inputs=inputs,
                          outputs=outputs,
                          title="Object Detection",
-                         #En la interfaz se pueden incluir ejemplos de lo que se espera como entrada o entradas. En este caso,
-                         # la entrada es una imagen por lo que se pueden poner imagenes de ejemplo (deben estar subidas en el repositorio
-                         # y con el path correctamente referenciado)
-                         examples=examples,
+                         
+                         #examples=examples,
                         )
 interface.launch()
