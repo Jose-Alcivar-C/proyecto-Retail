7f5e31f19d5da8ab84cb7cf22574c93f9c30a08b
José Alcivar C <josealci@espol.edu.ec>
2023-08-26 20:48:03 -0500

    version018

--- constants.py
+++ constants.py
@@ -1,2 +1,2 @@
-MODELO_ENTRENADO_PROPIO = "./modelos_finales/propio_yolov8x_test_02/weights/best.pt"
-MODELO_ENTRENADO_RETAIL = "./modelos_finales/retail_yolov8x_test_new_04/weights/best.pt"
+MODELO_ENTRENADO_PROPIO = "./modelos_finales/propio_yolov8x_test_01/weights/best.pt"
+MODELO_ENTRENADO_RETAIL = "./modelos_finales/retail_yolov8x_test_new_03/weights/last.pt"
